fd65fd34ea30506aedc678e136f62f8777d0c5d2
Kaustubh Salunkhe <kaustubhsalunkhe14@gmail.com>
2022-02-11 12:21:18 +0530

    second commit

--- appp.py
+++ appp.py
@@ -1,29 +1,32 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 import youtube_dl
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html')    
+    return render_template('about.html')
 
-@app.route('/download',methods=["POST","GET"])
+
+@app.route('/download', methods=["POST", "GET"])
 def download():
     url = request.form["url"]
-    print("Someone is trying to download",url)
+    print("Someone is trying to download", url)
     with youtube_dl.YoutubeDL() as ydl:
-        url=ydl.extract_info(url,download=False)
-        #print(url)
-        download_link=(url["formats"][-1]["url"])
+        url = ydl.extract_info(url, download=False)
+        print(url)
+        try:
+            download_link = url["entries"][-1]["formats"][-1]["url"]
+        except:
+            download_link = url["formats"][-1]["url"]
         return redirect(download_link+"&dl=1")
-       
-
-
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
